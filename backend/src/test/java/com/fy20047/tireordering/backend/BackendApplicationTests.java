9693972f6f4ab51d2a0ba4fd02d55d23da7a41ef
fy20047 <fy20001215@gmail.com>
2026-02-09 07:47:41 +0800

    Use H2 for Spring Boot context test

--- backend/src/test/java/com/fy20047/tireordering/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/fy20047/tireordering/backend/BackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.fy20047.tireordering.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // 確保只在測試時使用 H2
 class BackendApplicationTests {
 
 	@Test
