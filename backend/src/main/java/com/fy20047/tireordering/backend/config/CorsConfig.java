8996fe806b7c62d5d0b4450482fc4a4086f5c2ce
fy20047 <fy20001215@gmail.com>
2026-02-10 22:35:27 +0800

    Relax CORS for local ports

--- backend/src/main/java/com/fy20047/tireordering/backend/config/CorsConfig.java
+++ backend/src/main/java/com/fy20047/tireordering/backend/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // 這個規則適用於所有以 /api/ 開頭的網址
-                .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173") // 白名單 (Guest List)：Vite (React) 預設的啟動 Port
+                .allowedOriginPatterns("http://localhost:5173", "http://127.0.0.1:*") // 白名單 (Guest List)：Vite (React) 預設的啟動 Port
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 允許前端對後端做什麼動作
                 .allowedHeaders("*") // 允許前端帶任何 Header 過來（例如身分驗證的 Token）
                 .allowCredentials(true); // 允許帶 cookie (refresh token)，即允許 cookie 傳遞
